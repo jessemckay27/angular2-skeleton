2727a25416cef7e1e90e44941a2d91d932c5f7d0
Jesse <rosecity27@comcast.net>
2017-01-19 14:44:09 -0800

    now working, wrong tags in index.html

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,20 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-   <div class="container">
-      <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-      <h3>{{currentFocus}}</h3>
-  </div>
+  <h1>My First Angular 2 App</h1>
   `
 })
 
 export class AppComponent {
-  currentFocus: string = 'Angular Homework';
-  currentTime = new Date();
-  month: number = this.currentTime.getMonth() + 1;
-  day: number = this.currentTime.getDate();
-  year: number = this.currentTime.getFullYear();
-  firstTask = {
-    description: "Finish weekend Angular homework for Epicodus course"
-  }
+
 }
